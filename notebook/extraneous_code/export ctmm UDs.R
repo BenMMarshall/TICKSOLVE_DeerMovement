944ff7ebc91fad3835b5302ff0ee4f2ff4d1a182
BenMMarshall <benjaminmichaelmarshall@gmail.com>
2025-06-25 22:22:01 +0100

    UD exports

--- notebook/extraneous_code/export ctmm UDs.R
+++ notebook/extraneous_code/export ctmm UDs.R
@@ -4,9 +4,13 @@ library(ctmm)
 
 plot(tar_akdeLists$akde$Roe01_F)
 
-writeRaster(tar_akdeLists$akde$Roe01_F,
-            filename = here::here("modelOutput", "ctmmUD", "ctmmUD_Roe01.tif"),
-            format = "GTiff", DF = "CDF", overwrite = TRUE)
+for(indi in names(tar_akdeLists$akde)){
+  plot(tar_akdeLists$akde[[indi]])
+  writeRaster(tar_akdeLists$akde[[indi]],
+              filename = here::here("modelOutput", "ctmmUD", paste0("ctmmUD_", indi, ".tif")),
+              format = "GTiff", DF = "CDF", overwrite = TRUE)
+}
+
 
 library(terra)
 test <- terra::rast(here::here("modelOutput", "ctmmUD", "ctmmUD_Roe01.tif"))
